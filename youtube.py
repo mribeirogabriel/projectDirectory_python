7a641b6dd585b3330ee8a85b4dd5b12268c3b863
Marco <mribeirogabriel@hotmail.com>
2020-10-10 03:04:28 -0300

    tune adjusting

--- youtube.py
+++ youtube.py
@@ -2,17 +2,17 @@ import os
 
 def main():
     directory = os.path.dirname(__file__)
-    project = input('Insira o nome do Projeto: ')
+    project = input('[] Insert project name: ')
     projectDirectory = directory + '\\' + project
 
     def createDirectory():
         checkDirectory = os.path.exists(projectDirectory)
         
         if checkDirectory:
-            print ("Este projeto já existe: " + projectDirectory)
+            print ("[] This project already exists. " + projectDirectory)
             main()
         else:
-            print ("Criando diretorios do projeto: " + projectDirectory)
+            print ("[] Creating project directories: " + projectDirectory)
             os.mkdir(projectDirectory)
             os.mkdir(projectDirectory + '\\Audios')
             os.mkdir(projectDirectory + '\\Videos')
