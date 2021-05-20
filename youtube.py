9b59c88c7b1ca6230d8291259dc8d8f82da40f0f
marco <mribeirogabriel@gmail.com>
2021-05-20 00:42:03 -0300

    fix variables | EAFP | System detection

--- youtube.py
+++ youtube.py
@@ -1,25 +1,50 @@
 import os
 
+
 def main():
-    directory = os.path.dirname(__file__)
-    project = input('[] Insert project name: ')
-    projectDirectory = directory + '\\' + project
+    system_options = ['posix', 'nt']
+    system_detect = os.name
 
-    def createDirectory():
-        checkDirectory = os.path.exists(projectDirectory)
-        
-        if checkDirectory:
-            print ("[] This project already exists. " + projectDirectory)
-            main()
-        else:
-            print ("[] Creating project directories: " + projectDirectory)
-            os.mkdir(projectDirectory)
-            os.mkdir(projectDirectory + '\\Audios')
-            os.mkdir(projectDirectory + '\\Videos')
-            os.mkdir(projectDirectory + '\\Photos')
+    current_directory = os.path.dirname(__file__)
 
-            showDirectory = os.system('dir '+ projectDirectory)
-            print (showDirectory)
-            input("Press enter to continue")
-    createDirectory()
-main()
+    project_name = input('[] Insert project name: ')
+
+    def create_windows():
+        try:
+            project_directory = current_directory + '\\' + project_name
+            print("[] Creating project directories: " + project_directory)
+            os.mkdir(project_directory)
+            os.mkdir(project_directory + '\\Audios')
+            os.mkdir(project_directory + '\\Videos')
+            os.mkdir(project_directory + '\\Photos')
+
+            show_directory = os.system('dir ' + project_directory)
+            print(show_directory)
+            input("[] Press enter")
+        except FileExistsError:
+            print('[] Project already exists.')
+
+    def create_linux():
+        try:
+            project_directory = current_directory + project_name
+            print("[] Creating project directories: " + project_directory)
+            os.mkdir(project_directory)
+            os.mkdir(project_directory + '/' + 'Audios')
+            os.mkdir(project_directory + '/' + 'Videos')
+            os.mkdir(project_directory + '/' + 'Photos')
+
+            show_directory = os.system('ls ' + project_directory)
+            print(show_directory)
+            input("[] Press enter")
+        except FileExistsError:
+            print('[] Project already exists.')
+
+    if system_detect in str(system_options):
+        if system_detect == 'posix':
+            create_linux()
+        if system_detect == 'nt':
+            create_windows()
+
+
+if __name__ == '__main__':
+    main()
